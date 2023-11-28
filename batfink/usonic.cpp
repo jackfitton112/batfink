8a2451329cefeccb5ef477ee7a23bdfa763a0200
Jack Fitton <jackfitton01@gmail.com>
2023-11-28 18:12:45 +0000

    usonic tidy

---
+++ batfink/usonic.cpp
@@ -0,0 +1,116 @@
+/**
+ * @file usonic.cpp
+ * @author Jack Fitton (jf1595@york.ac.uk)
+ * @brief UltraSonic Sensor Functions
+ * @version 0.1
+ * @date 28-11-2023
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "usonic.h"
+
+
+mbed::DigitalInOut frontSensor(P0_23);
+mbed::DigitalInOut rightSensor(P1_14);
+mbed::DigitalInOut leftSensor(P1_13);
+
+
+// Define sensor data struct
+sensorDatas *sensorData = new sensorDatas;
+
+// Define sensorMutex variable
+rtos::Mutex sensorMutex;
+
+// Define sensorThread variable
+rtos::Thread sensorThread;
+
+
+void Usonic_setup(){
+    //set up the sensor thread
+    sensorThread.start(getSensorDataThread);
+}
+
+
+/**
+ * @brief Reads the distance from an ultrasonic sensor.
+ * 
+ * This function sends a trigger signal to the ultrasonic sensor, measures the time it takes for the echo signal to return,
+ * and calculates the distance based on the measured time.
+ * 
+ * @param sensor The digital input/output pin connected to the ultrasonic sensor.
+ * @return The distance measured by the ultrasonic sensor in centimeters.
+ */
+
+int ReadDistance(mbed::DigitalInOut sensor){
+    
+    // USONIC READ DISTANCE FUNCTION
+
+
+    sensor.output(); //Set pin as output
+
+    sensor = 0; //Set to low
+
+    wait_us(2); //Wait 2 microseconds
+
+    sensor = 1; //Set to high
+
+    wait_us(5); //Wait 5 microseconds
+
+    sensor = 0; //Set to low
+
+    wait_us(10); //Wait 10 microseconds
+
+    sensor.input(); //Set pin as input
+
+    while(sensor.read() == 0); //Wait until pin is high
+
+    mbed::Timer t; //Create timer object
+    t.start(); //Start timer
+
+    while(sensor.read() == 1); //Wait until pin is low
+
+    int time = t.read_us(); //Read time in microseconds
+
+    int distance = time / 58; //Calculate distance from time
+
+
+    t.stop(); //Stop timer
+    t.reset(); //Reset timer
+
+    return distance;
+ 
+}
+
+
+/**
+ * @brief This function runs in a separate thread to continuously read sensor data.
+ * 
+ * @details This function runs in an infinite loop and performs the following steps:
+ * 1. Reads the distance from the front, left, and right ultrasonic sensors.
+ * 2. Takes a mutex lock to ensure exclusive access to the sensor data.
+ * 3. Updates the sensor data struct with the new sensor values.
+ * 4. Releases the mutex lock.
+ * 5. Sleeps for 100 milliseconds before repeating the process.
+ */
+void getSensorDataThread(){
+    while(1){
+
+        //get the sensor values 
+        int front = ReadDistance(frontSensor);
+        int left = ReadDistance(leftSensor);
+        int right = ReadDistance(rightSensor);
+
+        //take mutex and set the sensor data
+        sensorMutex.lock();
+        sensorData->front = front;
+        sensorData->left = left;
+        sensorData->right = right;
+        sensorMutex.unlock();
+
+        //sleep for 50ms
+        rtos::ThisThread::sleep_for(100);
+
+    }
+}
--- batfink/usonic.h
+++ batfink/usonic.h
@@ -1,194 +1,27 @@
-rtos::Thread frontusonicThread;
-rtos::Thread leftusonicThread;
-rtos::Thread rightusonicThread;
+#ifndef USONIC_H
+#define USONIC_H
 
-rtos::Mutex frontusonicMutex;
-rtos::Mutex leftusonicMutex;
-rtos::Mutex rightusonicMutex;
-rtos::Mutex globalSensorMutex;
+#include <mbed.h>
 
-int front_distance = 0;
-int left_distance = 0;
-int right_distance = 0;
+// Declare sensor data struct
+struct sensorDatas {
+    int front;
+    int left;
+    int right;
+};
 
-mbed::DigitalInOut frontSensor(P0_23);
-mbed::DigitalInOut rightSensor(P1_14);
-mbed::DigitalInOut leftSensor(P1_13);
+// Declare sensor data extern variable
+extern sensorDatas *sensorData;
 
+// Declare sensorMutex extern variable
+extern rtos::Mutex sensorMutex;
 
+// Declare sensorThread extern variable
+extern rtos::Thread sensorThread;
 
-void frontusonicRead() {
-    while (1) {
-        
-        //get global sensor mutex
-        globalSensorMutex.lock();
+// Function prototypes
+void Usonic_setup();
+int ReadDistance(mbed::DigitalInOut sensor);
+void getSensorDataThread();
 
-        // Read distance from the front sensor
-        frontSensor.output();
-        frontSensor = 0;  // Set the pin low for 2 microseconds
-        wait_us(2);
-        frontSensor = 1;  // Set the pin high for 15 microseconds
-        wait_us(15);
-        frontSensor = 0;  // Set the pin low
-        frontSensor.input();  // Set the pin to input
-        wait_us(20);  // Wait for the pulse to start
-
-        mbed::Timer timer;
-        timer.start();
-
-        // Wait for the pulse to end
-        while (frontSensor.read() == 0 && timer.read_us() < 5000) {
-        }
-
-        timer.reset();
-
-        // Measure the pulse duration
-        while (frontSensor.read() == 1 && timer.read_us() < 5000) {
-        }
-
-        timer.stop();
-
-        //release global sensor mutex
-        globalSensorMutex.unlock();
-
-        // Acquire the mutex to prevent conflicts with other threads
-        frontusonicMutex.lock();
-
-        // Calculate distance based on the pulse duration
-        front_distance = static_cast<int>(timer.read_us() * 0.0343 / 2);
-
-        // Release the mutex
-        frontusonicMutex.unlock();
-
-
-
-        // Sleep for a while to control the sampling rate
-        rtos::ThisThread::sleep_for(100);  // Sleep for 100 milliseconds (adjust as needed)
-    }
-}
-
-void leftusonicRead() {
-
-        while (1) {
-
-
-        //get global sensor mutex
-        globalSensorMutex.lock();
-        
-        // Read distance from the left sensor
-        leftSensor.output();
-        leftSensor = 0;  // Set the pin low for 2 microseconds
-        wait_us(2);
-        leftSensor = 1;  // Set the pin high for 15 microseconds
-        wait_us(15);
-        leftSensor = 0;  // Set the pin low
-        leftSensor.input();  // Set the pin to input
-        wait_us(20);  // Wait for the pulse to start
-
-        mbed::Timer timer;
-        timer.start();
-
-        // Wait for the pulse to end
-        while (leftSensor.read() == 0 && timer.read_us() < 5000) {
-        }
-
-        timer.reset();
-
-        // Measure the pulse duration
-        while (leftSensor.read() == 1 && timer.read_us() < 5000) {
-        }
-
-        timer.stop();
-
-        //release global sensor mutex
-        globalSensorMutex.unlock();
-
-        // Acquire the mutex to prevent conflicts with other threads
-        leftusonicMutex.lock();
-
-        // Calculate distance based on the pulse duration
-        left_distance = static_cast<int>(timer.read_us() * 0.0343 / 2);
-
-        // Release the mutex
-        leftusonicMutex.unlock();
-
-
-
-        // Sleep for a while to control the sampling rate
-        rtos::ThisThread::sleep_for(100);  // Sleep for 100 milliseconds (adjust as needed)
-    }
-
-}
-
-void rightusonicRead() {
-
-        while (1) {
-
-        
-        //get global sensor mutex
-        globalSensorMutex.lock();
-        
-        // Read distance from the right sensor
-        rightSensor.output();
-        rightSensor = 0;  // Set the pin low for 2 microseconds
-        wait_us(2);
-        rightSensor = 1;  // Set the pin high for 15 microseconds
-        wait_us(15);
-        rightSensor = 0;  // Set the pin low
-        rightSensor.input();  // Set the pin to input
-        wait_us(20);  // Wait for the pulse to start
-
-        mbed::Timer timer;
-        timer.start();
-
-        // Wait for the pulse to end
-        while (rightSensor.read() == 0 && timer.read_us() < 5000) {
-        }
-
-        timer.reset();
-
-        // Measure the pulse duration
-        while (rightSensor.read() == 1 && timer.read_us() < 5000) {
-        }
-
-        timer.stop();
-
-        //release global sensor mutex
-        globalSensorMutex.unlock();
-
-        // Acquire the mutex to prevent conflicts with other threads
-        rightusonicMutex.lock();
-
-        // Calculate distance based on the pulse duration
-        right_distance = static_cast<int>(timer.read_us() * 0.0343 / 2);
-
-        // Release the mutex
-        rightusonicMutex.unlock();
-
-
-
-        // Sleep for a while to control the sampling rate
-        rtos::ThisThread::sleep_for(100);  // Sleep for 100 milliseconds (adjust as needed)
-    }
-
-}
-
-int* readSensorData() {
-    // get all sensor values and put them in an array
-    static int sensorData[3];  // static to ensure it persists outside the function
-
-    // take mutexes and get sensor values
-    frontusonicMutex.lock();
-    sensorData[0] = front_distance;
-    frontusonicMutex.unlock();
-
-    leftusonicMutex.lock();
-    sensorData[1] = left_distance;
-    leftusonicMutex.unlock();
-
-    rightusonicMutex.lock();
-    sensorData[2] = right_distance;
-    rightusonicMutex.unlock();
-
-    return sensorData;
-}
+#endif
